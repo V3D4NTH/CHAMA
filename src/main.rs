424160227f2bea1ce5c75dc782d11d103ef20f52
V3D4NTH <vedanth.sameer@gmail.com>
2024-07-22 23:01:07 +0530

    Extract URL path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::Write;
+use std::io::{
+    Read,
+    Write
+};
 use std::net::TcpListener;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,7 +13,21 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                let mut buffer = [0; 512];
+                stream.read(&mut buffer).unwrap();
+
+                let request = String::from_utf8_lossy(&buffer[..]);
+                let request_line = request.lines().next().unwrap();
+
+                let url_path = request_line.split_whitespace().nth(1).unwrap();
+
+                let response = if url_path == "/" {
+                    "HTTP/1.1 200 OK\r\n\r\n"
+                } else {
+                    "HTTP/1.1 404 Not Found\r\n\r\n"
+                };
+
+                let _ = stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
